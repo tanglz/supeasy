e728d2b1e19cd2b7c5ea559dae9f51032c20745e
Lisa Tang <lisatang@MacBook-Pro-3.local>
2021-02-17 10:39:16 -0500

    init project

---
+++ src/main/java/com/uoit/network/supeasy/SupeasyApplication.java
@@ -0,0 +1,13 @@
+package com.uoit.network.supeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uoit/network/supeasy/SupeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoit.network.supeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
